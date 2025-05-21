e561a556cf08ce3fdb2062c4969534cec133808d
youzhiyang <1454900087@qq.com>
2025-05-21 16:28:09 +0800

    MINOR: 代码微调

--- examples/Server/Hotfix/System/System_AuthenticationAccount.cs
+++ examples/Server/Hotfix/System/System_AuthenticationAccount.cs
@@ -1,4 +1,5 @@
-﻿using Fantasy.Async;
+﻿using Fantasy;
+using Fantasy.Async;
 using Fantasy.DataBase;
 using Fantasy.Entitas;
 using Fantasy.Helper;
@@ -15,30 +16,31 @@ public static class System_AuthenticationAccount {
         if (string.IsNullOrEmpty(account) || string.IsNullOrEmpty(password)) {
             return 1000; // 账号或是密码为空
         }
-        if (self._dicAllAccountCache.ContainsKey(account)) {
-            return 1001; // 账号已经存在
+        var accountHashCode = account.GetHashCode();
+        using (await self.Scene.CoroutineLockComponent.Wait(accountHashCode, accountHashCode, "Regitser Account")) {
+            
+            var dataBase = self.Scene.World.DataBase;
+            
+            if (self._dicAllAccountCache.ContainsKey(account)) {
+                return 1001; // 账号已经存在
+            }
+            var hasExist = await dataBase.Exist<Model_Account>(modelAccount => modelAccount.account == account);
+            if (hasExist) {
+                return 1001;
+            }
+
+            Model_Account accountModel = Entity.Create<Model_Account>(self.Scene, true, false);
+            accountModel.account = account;
+            accountModel.password = self.Scene.GetComponent<Component_RSAEncrypt>().EncryptPassword(password);
+            accountModel.createTime = TimeHelper.Now;
+
+            if (!self._dicAllAccountCache.TryAdd(accountModel.account, accountModel)) {
+                return 1001;
+            }
+
+            // 回写数据库
+            await dataBase.Save<Model_Account>(accountModel);
+            return 0; // 注册成功
         }
-
-        // TODO 数据库查询 是否哟
-        IDataBase dataBase = self.Scene.World.DataBase;
-        var hasExist = await dataBase.Exist<Model_Account>(modelAccount => modelAccount.account == account);
-        if (hasExist) {
-            return 1001;
-        }
-
-        // 
-        Model_Account accountModel = Entity.Create<Model_Account>(self.Scene, true, false);
-        accountModel.account = account;
-        accountModel.password = self.Scene.GetComponent<Component_RSAEncrypt>().EncryptPassword(password);
-        accountModel.createTime = TimeHelper.Now;
-
-        if (!self._dicAllAccountCache.TryAdd(accountModel.account, accountModel)) {
-            return 1001;
-        }
-
-        // 回写数据库
-        await dataBase.Save<Model_Account>(accountModel);
-
-        return 0; // 注册成功
     }
 }
