92ac2674d051fa28c4cb3f419cb919d458209f63
youzhiyang <1454900087@qq.com>
2025-05-21 15:19:52 +0800

    MINOR: Scene创建异步事件

---
+++ examples/Server/Hotfix/Events/Event_OnSceneCreate.cs
@@ -0,0 +1,27 @@
+﻿using Fantasy;
+using Fantasy.Async;
+using Fantasy.Event;
+using Fantasy.Model;
+
+namespace Hotfix;
+
+public class Event_OnSceneCreate : IAsyncEvent {
+    public Type EventType() {
+        return typeof(OnCreateScene);
+    }
+
+    public FTask InvokeAsync(object self) {
+        OnCreateScene onCreateScene = (OnCreateScene)self;
+        Scene scene = onCreateScene.Scene;
+        if (scene == null) {
+            Log.Error("self as Scene 是空值");
+        }
+        else {
+            if (scene.SceneType == (int)SceneType.Authentication) {
+                scene.AddComponent<Component_AuthenticationAccount>();
+                Log.Info("鉴权服务添加组件: Component_AuthenticationAccount");
+            }
+        }
+        return FTask.Create();
+    }
+}
