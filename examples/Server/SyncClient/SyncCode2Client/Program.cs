63e83917134ef7515e9a6f62aaf7a2f8ff53e556
youzhiyang <1454900087@qq.com>
2025-05-26 15:45:51 +0800

    MINOR: 将服务端文件共享给客户端

---
+++ examples/Server/SyncClient/SyncCode2Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+class Program {
+    static void Main(string[] args) {
+        var targetDir = new DirectoryInfo(Assembly.GetExecutingAssembly().FullName)
+            .Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.Parent.FullName;
+
+        var sourceDir = new DirectoryInfo(Assembly.GetExecutingAssembly().FullName)
+            .Parent.Parent.Parent.Parent.Parent.Parent.FullName;
+        targetDir = Path.Combine(targetDir, "Unity/Client/Assets/GameScripts/GlobalConfig");
+        sourceDir = Path.Combine(sourceDir, "Hotfix/ShareToClient");
+        Console.WriteLine(targetDir);
+        Console.WriteLine(sourceDir);
+
+        foreach (var file in Directory.GetFiles(sourceDir)) {
+            if (file.EndsWith(".cs")) {
+                var fileInfo = new FileInfo(file);
+                var targetFile = Path.Combine(targetDir, fileInfo.Name);
+                File.Copy(file, targetFile, true);
+                Console.WriteLine($"{file} => {targetFile}");
+            }
+        }
+    }
+}
